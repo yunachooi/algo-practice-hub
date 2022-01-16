5edba37530539886422651564131e6e16552e649
dev-glasodyyn <glasodyyn@gmail.com>
2022-01-17 00:29:01 +0900

    A+B -4

---
+++ BaekJoon/while문/B10951.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class B10951 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in=new Scanner(System.in);
+		
+		while(in.hasNextInt()){
+		
+			int a=in.nextInt();
+			int b=in.nextInt();
+			System.out.println(a+b);
+		
+		}	
+		in.close();
+	}
+
+}
