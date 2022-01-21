14c2cb333c224183d6e549c4597f55423d68e959
dev-glasodyyn <glasodyyn@gmail.com>
2022-01-21 23:13:57 +0900

    최댓값

--- BaekJoon/1차원 배열/B2562.java
+++ BaekJoon/1차원 배열/B2562.java
@@ -2,26 +2,26 @@ import java.util.Scanner;
 
 public class B2562 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		
-		int arr[] = new int[9];
-		int max = 0, max_index = 0;
-		
-		for (int i = 0; i < arr.length; i++)
-			arr[i]=sc.nextInt();
-		sc.close();
-		
-		for (int i = 0; i < arr.length; i++) {
-			if(arr[i] > max) {
-				max = arr[i];
-				max_index = i;
-			}
-		}
-		
-		System.out.println(max);
-		System.out.println(max_index+1);
-	}
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Scanner sc = new Scanner(System.in);
+
+        int arr[] = new int[9];
+        int max = 0, max_index = 0;
+
+        for (int i = 0; i < arr.length; i++)
+            arr[i]=sc.nextInt();
+        sc.close();
+
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] > max) {
+                max = arr[i];
+                max_index = i;
+            }
+        }
+
+        System.out.println(max);
+        System.out.println(max_index+1);
+    }
 
 }
