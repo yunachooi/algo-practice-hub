29d60f976c050993075e63551327aa717bab3337
dev-glasodyyn <glasodyyn@gmail.com>
2022-01-24 20:28:36 +0900

    정수 1개 입력받아 그대로 출력하기3

---
+++ CodeUp/데이터형/C1030.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    long long int n; //정수의 범위 -9,223,372,036,854,775,808 ~ +9,223,372,036,854,775,807
+
+    scanf("%lld", &n);
+
+    printf("%lld", n);
+
+    return 0;
+}
