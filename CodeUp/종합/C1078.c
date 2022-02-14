50c34a0290c159e444fcf700baf97185bdcc2c30
dev-glasodyyn <glasodyyn@gmail.com>
2022-02-15 00:14:32 +0900

    짝수 합 구하기

---
+++ CodeUp/종합/C1078.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int sum = 0;
+    int i, n;
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++)
+        if(i % 2 == 0)
+            sum += i;
+    printf("%d", sum);
+
+    return 0;
+}
