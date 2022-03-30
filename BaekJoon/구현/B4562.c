00c1aacd765772002980f1247762a040ee413801
dev-glasodyyn <glasodyyn@gmail.com>
2022-03-31 00:01:06 +0900

    No Brainer

---
+++ BaekJoon/구현/B4562.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int n, x, y;
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++){
+        scanf("%d %d", &x, &y);
+        if(x < y)
+            printf("NO BRAINS\n");
+        else
+            printf("MMM BRAINS\n");
+    }
+
+    return 0;
+}
