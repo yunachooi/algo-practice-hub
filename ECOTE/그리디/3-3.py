a743ee60458b86f6b9ae22f2df22ecc5fe8da82a
glasodyyn <glasodyyn@gmail.com>
2022-06-24 00:02:58 +0900

    숫자 카드 게임

---
+++ ECOTE/그리디/3-3.py
@@ -0,0 +1,11 @@
+n, m = map(int, input().split())
+result = 0
+
+for i in range(n):
+    data = list(map(int, input().split()))
+    # 현재 줄에서 가장 작은 수
+    min_value = min(data)
+    # 가장 작은 수 중에서 가장 큰 수
+    result = max(result, min_value)
+
+print(result)
