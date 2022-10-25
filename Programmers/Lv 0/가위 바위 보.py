90773ef03bb0347607d11fbe5f011f8444f9fe31
glasodyyn <glasodyyn@gmail.com>
2022-10-26 00:03:52 +0900

    가위 바위 보

---
+++ Programmers/Lv 0/가위 바위 보.py
@@ -0,0 +1,11 @@
+def solution(rsp):
+    answer = ''
+    rsp = list(rsp)
+    for i in range(len(rsp)):
+        if rsp[i] == '2':
+            answer += '0'
+        elif rsp[i] == '0':
+            answer += '5'
+        elif rsp[i] == '5':
+            answer += '2'
+    return answer
