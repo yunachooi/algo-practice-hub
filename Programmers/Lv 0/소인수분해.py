cd44b54198b325711f2fc1cd5663a47b5411d2e9
glasodyyn <glasodyyn@gmail.com>
2022-10-27 00:02:15 +0900

    소인수분해

---
+++ Programmers/Lv 0/소인수분해.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = []
+    i = 2
+    
+    while n != 1:
+        if n % i == 0:
+            if i not in answer:
+                answer.append(i)
+            n /= i
+        else:
+            i += 1
+    return answer
