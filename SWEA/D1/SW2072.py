70039ff497ad11ef6ddc02b9d8ad4074483b9d59
glasodyyn <glasodyyn@gmail.com>
2022-11-12 23:41:17 +0900

    홀수만 더하기

--- SWEA/D1/SW2072.py
+++ SWEA/D1/SW2072.py
@@ -1,8 +1,6 @@
 T = int(input())
 
 for test_case in range(1, T + 1):
-        numbers = list(map(int, input().split()))
-        
-        odd = [num for num in numbers if num % 2 == 1]
- 
-        print('#{} {}'.format(test_case, sum(odd)))
+    numbers = list(map(int, input().split()))
+    odd = [num for num in numbers if num % 2 == 1]
+    print('#{} {}'.format(test_case, sum(odd)))
