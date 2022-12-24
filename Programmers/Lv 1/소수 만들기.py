4cde37616ff8175dbe863a44159d0f3d4fc1ed04
glasodyyn <glasodyyn@gmail.com>
2022-12-25 00:18:20 +0900

    소수 만들기

---
+++ Programmers/Lv 1/소수 만들기.py
@@ -0,0 +1,14 @@
+def solution(nums):
+    from itertools import combinations as cb
+    answer = 0
+
+    for i in cb(nums, 3):
+        cand = sum(i)
+
+        for j in range(2, cand):
+            if cand % j == 0:
+                break
+            
+        else:
+            answer += 1
+    return answer
