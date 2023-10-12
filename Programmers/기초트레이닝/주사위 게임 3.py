8d0b35682192f15e916a304f6b1d90449239aeda
glasodyyn <glasodyyn@gmail.com>
2023-10-12 21:08:22 +0900

    주사위 게임 3

---
+++ Programmers/기초트레이닝/주사위 게임 3.py
@@ -0,0 +1,14 @@
+def solution(a, b, c, d):
+    l = [a,b,c,d]
+    c = [l.count(x) for x in l]
+    if max(c) == 4:
+        return 1111*a
+    elif max(c) == 3:
+        return (10*l[c.index(3)]+l[c.index(1)])**2
+    elif max(c) == 2:
+        if min(c) == 1:
+            return eval('*'.join([str(l[i]) for i, x in enumerate(c) if x == 1]))
+        else:
+            return (max(l) + min(l)) * abs(max(l) - min(l))
+    else:
+        return min(l)
