2aedd5ac029819394901a82a2b97259c26426f04
glasodyyn <glasodyyn@gmail.com>
2024-09-10 13:14:40 +0900

    다리 놓기

---
+++ BaekJoon/기본 수학 1/B1010.py
@@ -0,0 +1,15 @@
+import math
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+<<<<<<< HEAD
+for _ in range(t):
+  n, m = map(int, input().split())
+=======
+
+for _ in range(t):
+  n, m = map(int, input().split())
+  
+>>>>>>> a1998fbea06282610d84984f3d873cea38667df2
+  print(math.comb(m, n))
